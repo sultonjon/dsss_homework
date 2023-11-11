4aeee9ecab2a1359f0d2fe2588a656f802b38197
sultonjon <sulton.iskhakov@fau.de>
2023-11-11 22:19:21 +0100

    Required files(requirements.txt, sutup.py, __init__.py) to make repository pip installable are added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+#  from setuptools import find_packages
+
+setup(
+    name = "dsss_homework",
+    version="01",
+    author="Sulton",
+    author_email="sulton.iskhakov@fau.de",
+    install_requires = ["random", "unittest"]
+)
